16e7bdd34d4a353e71856a7efaf94eaf33e90e97
kylej19 <kmj19@tuta.io>
2026-03-04 21:38:23 -0600

    reusable-class-finishing-touches

--- daily/mar04/ArrayPractice.java
+++ daily/mar04/ArrayPractice.java
@@ -29,11 +29,14 @@ public class ArrayPractice {
 
 		System.out.println();
 
-		String cars[] = {"Ford","Toyota","Kia","Dodge"};
+		// arrays of strings, doubles
+		//String cars[] = {"Ford","Toyota","Kia","Dodge"};
 		double salary[] = {12.2,45.2,23.01};
-		System.out.println(cars[0]);
+		// element access like any other array
+		// System.out.println(cars[0]);
 		System.out.println();
 
+		// comparing arrays is not as straightforward
 		int arr1[] = {1,2,3};
 		int arr2[] = {1,2,3};
 
@@ -41,7 +44,47 @@ public class ArrayPractice {
 		System.out.println("arr2: " + arr2);
 		
 		System.out.println("arr1==arr2 --> " + (arr1==arr2));
+		// there is a dedicated method to compare from the Arrays class (java.util.Arrays) 
 		System.out.println("Arrays.equals(arr1,arr2) --> "+Arrays.equals(arr1,arr2));
-	}
 
+		System.out.println();
+		// we can have arrays of objects as well
+		// Student csStudents[] = { new Student("John",123), new Student("Doe",456) };
+
+		// our accessors can be used on the elements within the array
+	
+		// System.out.println(csStudents[0].getName());
+		// System.out.println(csStudents[1].getName());
+
+
+		System.out.println();
+		System.out.println();
+		
+		Scanner kb = new Scanner(System.in);
+
+		String cars[] = new String[3];
+		for (int i = 0; i < cars.length; i++) {
+			System.out.print("Enter car name: ");
+			cars[i] = kb.nextLine();
+		}
+		for (int i = 0; i < cars.length; i++) {
+			System.out.printf("car %d: %s ", i, cars[i]);
+		}
+
+		System.out.printf("\n");
+
+	}
+	// example 
+	class Student {
+		private String name;
+		private int id;
+
+		public Student(String name, int id) {
+			this.name = name;
+			this.id = id;
+		}
+		public String getName() {
+			return name;
+		}
+	}
 }
